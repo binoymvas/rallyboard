df264546b9bad1fef109771107490384882a57a5
binoy mv <binoy.mv@poornam.com>
2016-12-23 14:51:04 +0530

    clean up

--- horizon/rallyboard/dashboard.py
+++ horizon/rallyboard/dashboard.py
@@ -1,21 +1,25 @@
-# File Name: dashboard.py
-#
-# Software: Openstack Horizon [liberity]
-#
-# Start Date: 31th Aug 2016
-#
-# Copyright: 2016@nephoscale.com
+ _______________________________________________________________________
+# | File Name: dashboard.py                                             |
+# |                                                                     |
+# | This file is for handling the views of support ticket display       |
+# |_____________________________________________________________________|
+# | Start Date: Nov 16th, 2016                                          |
+# |                                                                     |
+# | Package: Openstack Horizon Dashboard [liberity]                     |
+# |                                                                     |
+# | Copy Right: 2016@nephoscale                                         |
+# |_____________________________________________________________________|
 
+#Importing the required packages
 from django.utils.translation import ugettext_lazy as _
 import horizon
 class RallyDashboard(horizon.Dashboard):
     """ 
-    Class to register the Customer Support dashboard to horizon
+    Class to register the Customer dashboard to horizon
     """
     name   = _("Rally Tests")
     slug   = "rally_dashboard"
     panels = ('tasks', 'test_reports', ) 
     default_panel = 'tasks'
     permissions = ('openstack.roles.admin',)
-
 horizon.register(RallyDashboard)
--- horizon/rallyboard/tasks/panel.py
+++ horizon/rallyboard/tasks/panel.py
@@ -1,18 +1,23 @@
-# File Name: panel.py
-#
-# Software: Openstack Horizon [liberity]
-#
-# Dashboard Name: Rally Borad
-#
-# Panel Name: tasks
-#
-# Start Date: 2016@nephoscale.com
+# _______________________________________________________________________
+# | File Name: panel.py                                                 |
+# |                                                                     |
+# | This file is for handling the views of support ticket display       |
+# |_____________________________________________________________________|
+# | Start Date: Nov 16th, 2016                                          |
+# |                                                                     |
+# | Package: Openstack Horizon Dashboard [liberity]                     |
+# |                                                                     |
+# | Copy Right: 2016@nephoscale                                         |
+# |_____________________________________________________________________|
 
+#Importing the packages
 from django.utils.translation import ugettext_lazy as _
 from openstack_dashboard.dashboards.rallyboard import dashboard
 import horizon
 
 class Tasks(horizon.Panel):
     name = _("Tasks")
-    slug = "tasks"	
+    slug = "tasks"
+    
+#Registering the class
 dashboard.RallyDashboard.register(Tasks)
